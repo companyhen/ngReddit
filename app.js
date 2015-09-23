279abc531494c7ea3019182723e4e3c32c647ef4
companyhen <companyhen@gmail.com>
2015-09-23 15:18:15 -0400

    added loading-bar external dependency

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var app = angular.module('ngReddit', ['ngRoute']);
+var app = angular.module('ngReddit', ['ngRoute', 'angular-loading-bar']);
 
 app.config(function($routeProvider){
 	$routeProvider
